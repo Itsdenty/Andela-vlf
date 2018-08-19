30d32d7833ec5c7e7b761a7ccf05cb942145ba39
abd-afeez <dent4real@yahoo.com>
2018-08-19 12:38:19 +0100

    Update folder structure

--- api/facet.js
+++ api/facet.js
@@ -1,5 +1,5 @@
 import resource from 'resource-router-middleware';
-import facets from '../models/facets';
+import facets from '../Database/facets';
 
 export default ({ config, db }) => resource({
 
